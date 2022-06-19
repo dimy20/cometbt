402436a50c3d5b3b725a2d1573e1e46a4e362ef9
dimy20 <enzostrindade16@outlook.com>
2022-06-19 15:59:36 -0300

    becode parser, following wikipedia

--- main.cpp
+++ main.cpp
@@ -1,29 +1,165 @@
 #include <vector>
 #include <iostream>
-#include <GL/glew.h>
-#include "display.h"
-#include "shader.h"
-#include "mesh.h"
+
+enum class becode_type{
+	INT = 1,
+	STRING,
+	LIST,
+	DICT
+};
+
+becode_type get_type(char c){
+	switch(c){
+		case 'i':
+			return becode_type::INT;
+		case 'd':
+			return becode_type::DICT;
+		case 'l':
+			return becode_type::LIST;
+		default:
+			if(std::isdigit(c) != 0) return becode_type::STRING;
+	}
+}
+std::string becode_string(std::string& s, int offset, int n){
+	std::string ans = "";
+	for(int i = offset; i < offset + n; i++){
+		ans += s[i];
+	}
+	return ans;
+}
+
+int becode_int(const std::string& s, int& i){
+	std::string ans = "";
+	while(s[i] != 'e'){
+		ans += s[i];
+		i++;
+	}
+	return std::stoi(ans);
+}
+
+std::vector<std::string> parse_list(std::string& s, int& i){
+	std::vector<std::string> ans;
+	std::string elem = "";
+	int len;
+	while(s[i] != 'e'){
+		if(s[i] == 'i')
+			ans.push_back(std::to_string(becode_int(s, ++i)));
+		else if (std::isdigit(s[i]) != 0){
+			while(s[i] != ':'){
+				elem += s[i];
+				i++;
+			}
+			i++;
+			len = std::stoi(elem);
+			ans.push_back(becode_string(s, i, len));
+			i += len - 1;
+			elem = "";
+		}
+		i++;
+	}
+	return ans;
+}
+
+std::string get_key(std::string& s, int& i){
+	int len;
+	std::string elem = "";
+	std::string ans;
+	if(std::isdigit(s[i])){
+		while(s[i] != ':'){
+			elem += s[i];
+			i++;
+		}
+		i++;
+		len = std::stoi(elem);
+	}
+	ans = becode_string(s, i, len);
+	i += len;
+	return ans;
+}
+
+int get_str_len(std::string& s, int& i){
+	std::string len_s = "";
+	while(s[i] != ':'){
+		len_s += s[i];
+		i++;
+	}
+	i++;
+	return std::stoi(len_s);
+}
+
+std::vector<std::string> parse_bencode(std::string s){
+	int n;
+	n = s.size();
+	std::vector<std::string> ans;
+	std::string elem = "";
+	int len;
+	for(int i = 0; i < n; ++i){
+		if(s[i] == 'i'){
+			ans.push_back(std::to_string(becode_int(s, ++i)));
+			elem = "";
+		}else if (std::isdigit(s[i]) != 0){
+			while(s[i] != ':'){
+				elem += s[i];
+				i++;
+			}
+			i++;
+			len = std::stoi(elem);
+			ans.push_back(becode_string(s, i, len));
+			i += len-1;
+			elem = "";
+		}else if (s[i] == 'l'){ 
+			std::string list = "list [";
+			i++;
+			for(auto e : parse_list(s, i)){
+				list += e + ", ";
+			}
+			list += "]";
+			ans.push_back(list);
+		}else if(s[i] == 'd'){
+			i++;
+			while(i < n && s[i] != 'e'){
+				std::string key, value;
+				key = get_key(s, i);
+				becode_type type = get_type(s[i]);
+				std::cout << i << std::endl;
+				switch(type){
+					case becode_type::INT:
+						i++; /*skip i char*/
+						value = std::to_string(becode_int(s, i));
+						break;
+					case becode_type::STRING:
+						len = get_str_len(s, i);
+						value = becode_string(s, i, len);
+						i += len-1;
+						break;
+					case becode_type::LIST:
+						i++;
+						std::string list = "list [";
+						for(auto e : parse_list(s, i)){
+							list += e + ", ";
+						}
+						value = list + "]";
+						break;
+
+				}
+				std::string entry = "";
+				entry += "{" + key + ":" + value + "}";
+				ans.push_back("dict " + entry);
+				i++;
+			}
+		}
+	}
+	return ans;
+}
 
 int main(){
-	Display display(800, 600, "hell world");
-	Shader shader("../shaders.shader");
-	Vertex vertices [] ={
-		Vertex(glm::vec3(-0.5,-0.5,0)),
-		Vertex(glm::vec3(0,0.5,0)),
-		Vertex(glm::vec3(0.5,-0.5,0)),
-	};
+	std::vector<std::string> vec;
 
-	Mesh mesh(vertices, 3);
+	std::string val;
+	while(std::cin >> val);
 
-	while(!display.isClosed()){
-		glClearColor(0.0f, 0.15f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		shader.bind();
-		mesh.draw();
-
-		display.Update();
-	}
+	//parse_bencode(val);
+	for(auto x : parse_bencode(val)) std::cout << x << std::endl;
 
 	return 0;
 }
