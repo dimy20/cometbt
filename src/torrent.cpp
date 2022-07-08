78bdc764f95ea78cb9fc83c3f098d67ed1976b36
dimy20 <enzostrindade16@outlook.com>
2022-07-07 23:47:42 -0300

    helper function to serialize int into buffer

--- src/torrent.cpp
+++ src/torrent.cpp
@@ -24,6 +24,14 @@ static int get_length(const char * const buff, std::size_t size){
 	return ans;
 };
 
+static void serialize_int(std::uint8_t * begin, std::uint8_t * end, int value){
+	if((end - begin + 1) < 4) return;
+	begin[3] = value & 0xff;
+	begin[2] = value >> 8 & 0xff;
+	begin[1] = value >> 16 & 0xff;
+	begin[0] = value >> 24 & 0xff;
+};
+
 static void die(const std::string& msg){
 	std::cerr << msg << std::endl;
 	exit(EXIT_FAILURE);
