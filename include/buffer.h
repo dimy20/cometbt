4289f222ac2fcf1cc28fafd0f3606e44a588edd7
dimy20 <enzostrindade16@outlook.com>
2022-07-11 13:23:55 -0300

    buffer acess

--- include/buffer.h
+++ include/buffer.h
@@ -3,14 +3,25 @@
 #include <cstddef>
 #include <climits>
 #include <assert.h>
+
 class Buffer{
 	public:
 		Buffer(std::size_t size);
 		Buffer(std::size_t size, char * src, std::size_t src_size); 
 		Buffer(Buffer && other);
+		Buffer& operator=(Buffer && other);
 		char * data();
 		char const * data() const;
 		std::size_t size();
+		bool empty();
+
+		char const& operator[](std::size_t index) const;
+		char& operator[](std::size_t index);
+
+		char * begin();
+		char const * begin() const;
+		char * end();
+		char const * end() const;
 		~Buffer();
 	private:
 		std::size_t m_size;
--- src/buffer.cpp
+++ src/buffer.cpp
@@ -21,6 +21,17 @@ Buffer::Buffer(Buffer && other): m_begin(other.m_begin), m_size(other.m_size){
 	other.m_size = 0;
 }
 
+// move assigment operator
+Buffer& Buffer::operator=(Buffer && other){
+	if(&other == this) return *this;
+	std::free(m_begin);
+	m_begin = other.m_begin;
+	m_size = other.m_size;
+	other.m_begin = nullptr;
+	other.m_size = 0;
+	return *this;
+};
+
 Buffer::~Buffer(){
 	std::free(m_begin);
 }
@@ -35,4 +46,34 @@ char const * Buffer::data() const{
 
 std::size_t Buffer::size(){
 	return m_size;
+}
+
+bool Buffer::empty(){
+	return m_size == 0;
+}
+
+char const& Buffer::operator[](std::size_t index) const{
+	assert(index <= m_size - 1);
+	return m_begin[index];
+}
+
+char& Buffer::operator[](std::size_t index){
+	assert(index <= m_size - 1);
+	return m_begin[index];
+
+}
+
+char const * Buffer::begin() const {
+	return m_begin;
+}
+
+char * Buffer::begin(){
+	return m_begin;
+}
+
+char * Buffer::end(){
+	return m_begin + m_size;
+}
+char const * Buffer::end() const{
+	return m_begin + m_size;
 }
