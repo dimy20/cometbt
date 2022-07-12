fc2581948180471c35851b81809a2f43b8895456
dimy20 <enzostrindade16@outlook.com>
2022-07-12 17:55:03 -0300

    separate peer from torrent

---
+++ include/peer_connection.h
@@ -0,0 +1,55 @@
+#pragma once
+#include "tcp.h"
+#include <vector>
+#define BUFF_SIZE 1024*32
+/* handshake macros */
+#define HANDSHAKE_SIZE 68
+#define RESERVED_BYTES_LENGTH 8
+#define INFO_HASH_LENGTH 20
+#define PEER_ID_LENGTH   20
+#define PROTOCOL_ID_LENGTH 19
+#define PROTOCOL_ID "BitTorrent protocol"
+
+/* message macros*/
+
+#define MESSAGE_LENGTH_SIZE 4 // length prefix on every message
+#define MESSAGE_ID_SIZE 1
+#define BLOCK_LENGTH 1024 * 16
+#define PIECE_INDEX_SIZE 4
+#define BLOCK_OFFSET_SIZE 4
+#define BLOCK_LENGTH_SIZE 4
+
+class Peer : public SocketTcp{
+	public:
+		Peer(std::vector<char> id, const std::string& ip, const std::string& port);
+		void send_handshake(const std::vector<unsigned char>& info_hash, const std::string& id);
+		bool wait_handshake();
+		bool has_piece(int index);
+public:
+	enum class p_state{
+		HANDSHAKE_WAIT = 1, /*handshake sent and waiting for response*/
+		HANDSHAKE_DONE = 2,  /*Successful handshake response received*/
+		HANDSHAKE_FAIL = 4,/*Incorrenct handshake response received*/
+		MESSAGE_HANDLING = 8,
+		MESSAGE_FINISHED = 9
+	};
+	p_state m_state;
+	std::vector<char> m_id;
+	std::string m_ip;
+	std::string m_port;
+	std::vector<unsigned char> m_info_hash;
+	// maybe make a message class
+	int m_msg_len;
+	int m_total;
+	char m_buff[BUFF_SIZE];
+	char * m_bitfield;
+	bool m_choked;
+};
+
+struct handshake_s{
+	char proto_id_size;
+	char protocol_id[PROTOCOL_ID_LENGTH];
+	char reserved_bytes[RESERVED_BYTES_LENGTH];
+	char info_hash[INFO_HASH_LENGTH];
+	char peer_id[PEER_ID_LENGTH];
+};
---
+++ src/peer_connection.cpp
@@ -0,0 +1,67 @@
+#include "peer_connection.h"
+
+bool Peer::has_piece(int peer_index){
+	int byte_offset, bit_offset;
+	byte_offset = peer_index / 8;
+	bit_offset = peer_index % 8;
+	return ((*(m_bitfield + byte_offset)) >> bit_offset) & 1;
+};
+
+bool Peer::wait_handshake(){
+	/* check if handshake has be done already*/
+	if(m_state == p_state::HANDSHAKE_DONE) return true;
+	else if(m_state == p_state::HANDSHAKE_WAIT){
+		char buff[HANDSHAKE_SIZE];
+		bool info_hash_match, peer_id_match;
+		info_hash_match = peer_id_match = false;
+		int n;
+
+		memset(buff, 0, HANDSHAKE_SIZE);
+		n = recv(buff, HANDSHAKE_SIZE);
+		std::cout << "peer " << m_ip << " sent " << n << " bytes." << std::endl;
+		//wait for handshake response
+		struct handshake_s * hs_reply;
+		hs_reply = reinterpret_cast<struct handshake_s *>(buff);
+
+		if(memcmp(hs_reply->info_hash, m_info_hash.data(), INFO_HASH_LENGTH) == 0)
+			info_hash_match = true;
+		if(memcmp(hs_reply->peer_id, m_id.data(), PEER_ID_LENGTH) == 0)
+			peer_id_match = true;
+
+		if(info_hash_match && peer_id_match){
+			m_state = Peer::p_state::HANDSHAKE_DONE;
+			return true;
+		}else{
+			close();
+			m_state = Peer::p_state::HANDSHAKE_FAIL;
+			return false;
+			// what about epoll??
+		}
+	// bitfield here?
+	}else return false;
+
+};
+
+Peer::Peer(std::vector<char> id, const std::string& ip, const std::string& port) : SocketTcp() {
+	m_id = std::move(id);
+	m_ip = std::move(ip);
+	m_port = std::move(port);
+	m_total = 0;
+	memset(m_buff, 0, BUFF_SIZE);
+	m_choked = true;
+};
+
+void Peer::send_handshake(const std::vector<unsigned char>& info_hash, const std::string& id){
+	assert(get_fd() != -1);
+
+	struct handshake_s hs;
+	hs.proto_id_size = 19;
+	memset(&hs.protocol_id, 0, sizeof(hs) - 1);
+	memcpy(hs.protocol_id, PROTOCOL_ID, PROTOCOL_ID_LENGTH);
+	memcpy(hs.info_hash, info_hash.data(), INFO_HASH_LENGTH);
+	memcpy(hs.peer_id, id.c_str(), PEER_ID_LENGTH);
+
+	send(reinterpret_cast<char *>(&hs), HANDSHAKE_SIZE);
+	m_info_hash = info_hash; // copy to verify later when handshake response comes
+	m_state = p_state::HANDSHAKE_WAIT;
+};
