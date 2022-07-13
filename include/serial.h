bf1092a41924b217bd5d47e2f85960b15aea6685
dimy20 <enzostrindade16@outlook.com>
2022-07-13 10:50:45 -0300

    deserialize int

--- include/serial.h
+++ include/serial.h
@@ -6,4 +6,8 @@ namespace aux{
 	bool is_little_endian();
 	// serialize an int into a buffer in network ordering
 	void serialize_int(std::uint8_t * begin, std::uint8_t * end, int value);
+	// deserialize int in buffer stored in big-endian bye ordering to local int.
+	// deserialize_int expects the integer to be stored in the buffer in big-endian 
+	// format
+	int deserialize_int(const char * begin, const char * end);
 };
--- src/serial.cpp
+++ src/serial.cpp
@@ -12,3 +12,23 @@ void aux::serialize_int(std::uint8_t * begin, std::uint8_t * end, int value){
 	begin[1] = value >> 16 & 0xff;
 	begin[0] = value >> 24 & 0xff;
 }
+
+int aux::deserialize_int(const char * begin, const char * end){
+	std::ptrdiff_t diff = end - begin;
+	if(diff < 4) return -1;
+	int ans;
+	if(is_little_endian){
+		//store as least significant byte first
+		ans = static_cast<int>((unsigned char)begin[0] << 24 |
+							   (unsigned char)begin[1] << 16 |
+							   (unsigned char)begin[2] << 8  |
+							   (unsigned char)begin[3]);
+	}else{
+		// store as least significat byte last (same as buff)
+		ans = static_cast<int>((unsigned char)begin[3] << 24 |
+							   (unsigned char)begin[2] << 16 |
+							   (unsigned char)begin[1] << 8  |
+							   (unsigned char)begin[0]);
+	}
+	return ans;
+};
