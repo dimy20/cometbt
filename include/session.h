6446a188acdd7e5a5768c3a454f0a4fa4583bfa2
dimy20 <enzostrindade16@outlook.com>
2022-07-13 15:49:15 -0300

    session object will be responsible for handling the download

---
+++ include/session.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <vector>
+#include "event_loop.h"
+#include "peer_connection.h"
+#include "torrent.h"
+
+// class responsible for managing the peer connections
+// holds event loop
+// this class will stand high in the layer hierarchy
+// we will probably have some other session class reponsible for working with peer's
+// socket which this class will derive from, for now we will keep everything coupled
+// together here until i decide to abstract down this functionality as the class
+// grows.
+// Peer connection need peer_info to populate its internal fields, how to get
+// that from torrent?
+// session now only supports one torrent and one set of peers for that torrent
+class Session{
+	public:
+		Session(const std::string& filename);
+		void add_torrent(const std::string& filename);
+		void start();
+	private:
+		Torrent m_torrent; // make this better
+		std::vector<PeerConnection> m_peer_connections;
+		EventLoop m_main_loop; // main loop
+		int x;
+};
---
+++ src/session.cpp
@@ -0,0 +1,42 @@
+#include "session.h"
+
+static void read_cb(SocketTcp * sock){
+	PeerConnection * peer = dynamic_cast<PeerConnection *>(sock);
+	peer->on_receive_data();
+};
+
+static std::vector<char> open_file(const std::string& filename){
+	std::vector<char> buff;
+	int size;
+
+	std::ifstream file(filename, std::ios::binary | std::ios::ate);
+	size = file.tellg();
+	file.seekg(0, std::ios::beg);
+	buff.resize(size);
+
+	file.read(buff.data(), size);
+	return buff;
+};
+
+Session::Session(const std::string& filename){
+	auto torrent_file = open_file(filename);
+	m_torrent.set(std::move(torrent_file));
+	m_torrent.init_torrent_data();
+
+	// populates peers infos
+	m_torrent.get_peers();
+}
+
+// should only be responsible for handling main loop for now
+void Session::start(){
+	// client hadles just one peer for now, this will change when buffer layers are
+	// added and bandwidth manager is implemented
+	
+	// add one connection
+	m_peer_connections.push_back(PeerConnection(m_torrent.get_peers_infos()[0]));
+	auto peer = m_peer_connections[0];
+	peer.start();
+	// watch one connection
+	m_main_loop.watch(&peer, EventLoop::ev_type::READ, read_cb);
+	m_main_loop.run();
+};
