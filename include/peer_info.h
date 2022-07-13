d58e2e38befccb57544d5e70722607bcb22cdcea
dimy20 <enzostrindade16@outlook.com>
2022-07-13 14:28:26 -0300

    peer_info_s

--- include/peer_info.h
+++ include/peer_info.h
@@ -2,7 +2,9 @@
 #include <string>
 #include <vector>
 struct peer_info_s{
-	std::vector<char> m_id; // peer id
+	std::vector<char> m_remote_id; // peer id
 	std::string m_remote_ip; // remote end of this peer
-	std::string m_port; 
+	std::string m_remote_port;
+	std::string m_id; // my id
+	std::vector<unsigned char> m_info_hash; // move this out of here to a torrent_info
 };
