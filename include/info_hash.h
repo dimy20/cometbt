4ed73682a00c66bf0f85ea9e6c75fadaa949ce2b
dimy20 <enzostrindade16@outlook.com>
2022-07-20 13:14:34 -0300

    info_hash

--- include/info_hash.h
+++ include/info_hash.h
@@ -2,14 +2,33 @@
 
 #include <string>
 #include <sstream>
+#include <string.h>
 #include <openssl/sha.h>
 #include <openssl/crypto.h>
 
-class info_hash{
-	public:
-		info_hash(char * begin, std::size_t size);
-		std::string to_string_hex();
+//todo -> add copy constructor
+namespace aux{
+	class info_hash{
+		public:
+			info_hash() = default;
+			info_hash(char * begin, std::size_t size);
+			info_hash(info_hash && other);
+			~info_hash();
+			info_hash& operator=(const info_hash& other);
 
-	private:
-		unsigned char m_sha1_hash[SHA_DIGEST_LENGTH];
-};
+			
+			std::string hex_str();
+			bool operator==(const info_hash& other);
+			bool operator!=(const info_hash& other);
+
+			// populates buffer with pre-computed sha1 hash, if buffer contains
+			// a sha1 digest already it will be overwritten.
+			void set(char * begin, std::size_t size);
+			// get raw sha1 digest
+			const unsigned char * get() const { return m_sha1_hash; };
+
+		private:
+			unsigned char * m_sha1_hash = nullptr;
+	};
+}
+
--- src/info_hash.cpp
+++ src/info_hash.cpp
@@ -1,11 +1,14 @@
 #include "info_hash.h"
-
-info_hash::info_hash(char * begin, std::size_t size){
+#include <iostream>
+aux::info_hash::info_hash(char * begin, std::size_t size){
+	m_sha1_hash = (unsigned char *)std::malloc(SHA_DIGEST_LENGTH);
+	if(m_sha1_hash == nullptr) throw std::bad_alloc();
 	SHA1(reinterpret_cast<unsigned char*>(begin), size, m_sha1_hash);
 };
 
 
-std::string info_hash::to_string_hex(){
+std::string aux::info_hash::hex_str(){
+	if(m_sha1_hash == nullptr) return "";
 	char sha1_hex[(SHA_DIGEST_LENGTH * 2) + 1];
 	for (int i = 0; i < SHA_DIGEST_LENGTH; i++){
 		sprintf(sha1_hex + (i * 2),"%02x\n", m_sha1_hash[i]);
@@ -20,4 +23,42 @@ std::string info_hash::to_string_hex(){
 	};
 
 	return ss.str();
+};
+
+bool aux::info_hash::operator==(const info_hash& other){
+	return memcmp(m_sha1_hash, other.m_sha1_hash, SHA_DIGEST_LENGTH) == 0;
+};
+
+bool aux::info_hash::operator!=(const info_hash& other){
+	return memcmp(m_sha1_hash, other.m_sha1_hash, SHA_DIGEST_LENGTH) != 0;
+}
+
+aux::info_hash::~info_hash(){
+	std::free(m_sha1_hash);
+};
+
+aux::info_hash& aux::info_hash::operator=(const info_hash& other){
+	m_sha1_hash = reinterpret_cast<unsigned char*>(std::malloc(SHA_DIGEST_LENGTH));
+	if(!m_sha1_hash) throw std::bad_alloc();
+	memcpy(m_sha1_hash, other.m_sha1_hash, SHA_DIGEST_LENGTH);
+	return *this;
+};
+
+
+aux::info_hash::info_hash(info_hash && other){
+	m_sha1_hash = other.m_sha1_hash;
+	other.m_sha1_hash = nullptr;
+};
+
+void aux::info_hash::set(char * begin, std::size_t size){
+	if(size > SHA_DIGEST_LENGTH){
+		std::cerr << "Error:invalid sha1 hash" << std::endl;
+		exit(1);
+	}else if(m_sha1_hash != nullptr){
+		memcpy(m_sha1_hash, begin, SHA_DIGEST_LENGTH);
+	}else{
+		m_sha1_hash = reinterpret_cast<unsigned char *>(malloc(SHA_DIGEST_LENGTH));
+		if(!m_sha1_hash) throw std::bad_alloc();
+		memcpy(m_sha1_hash, begin, SHA_DIGEST_LENGTH);
+	}
 };
