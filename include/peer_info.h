df2cbc89311df83aab4e57b2e562facaca8df1d9
dimy20 <enzostrindade16@outlook.com>
2022-07-20 13:15:22 -0300

    use info_hash

--- include/peer_info.h
+++ include/peer_info.h
@@ -2,12 +2,14 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include "info_hash.h"
 struct peer_info_s{
 	std::vector<char> m_remote_id; // peer id
 	std::string m_remote_ip; // remote end of this peer
 	std::string m_remote_port;
 	std::string m_id; // my id
-	std::vector<unsigned char> m_info_hash; // move this out of here to a torrent_info
+
+	aux::info_hash m_info_hash;
 
 	peer_info_s() = default;
 
@@ -16,6 +18,7 @@ struct peer_info_s{
 		m_remote_ip = std::move(other.m_remote_ip);
 		m_remote_port = std::move(other.m_remote_port);
 		m_id = std::move(other.m_id);
+
 		m_info_hash = std::move(other.m_info_hash);
 	}
 
@@ -26,6 +29,7 @@ struct peer_info_s{
 		m_remote_ip = other.m_remote_ip;
 		m_remote_port = other.m_remote_port;
 		m_id = other.m_id;
+
 		m_info_hash = other.m_info_hash;
 		return *this;
 	}
