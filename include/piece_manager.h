c38fc19aae004397e277fece31c2eebb7144741e
dimy20 <enzostrindade16@outlook.com>
2022-08-17 23:20:20 -0300

    work_queue

--- include/piece_manager.h
+++ include/piece_manager.h
@@ -11,14 +11,17 @@ class peer_connection;
 class piece_manager{
 	public:
 		piece_manager() = default;
-		piece_manager(std::vector<char>& piece_hashes);
+		piece_manager(long long piece_len, std::vector<char>& piece_hashes);
 		piece_manager& operator=(piece_manager && other);
 		//updates all pieces for this peer.
 		void update(const peer_connection * conn, const aux::bitfield& bf);
 		std::vector<piece> rarest_first();
 	private:
 		std::vector<piece> m_pieces;
-		std::priority_queue<int, std::vector<int>, std::greater<int>> m_heap;
+		std::queue<piece *> get_work_queue() { return m_work_queue; };
+
+	private:
+		std::queue<piece *> m_work_queue;
 };
 
 // whenever a bitfiled arrives at a peer, the peer will notify the pice manager
--- src/piece_manager.cpp
+++ src/piece_manager.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "piece_manager.h"
 
-piece_manager::piece_manager(std::vector<char>& piece_hashes){  
+piece_manager::piece_manager(long long piece_length, std::vector<char>& piece_hashes){
 	int n = piece_hashes.size() / 20 ;
 	m_pieces = std::vector<piece>(n);
 
@@ -9,13 +9,20 @@ piece_manager::piece_manager(std::vector<char>& piece_hashes){
 	// build pieces base
 	for(int i = 0; i < n; i++){
 		aux::info_hash piece_hash(piece_hashes.data() + offset, SHA_DIGEST_LENGTH);
-		m_pieces[i] = std::move(piece(i, std::move(piece_hash)));
+		m_pieces[i] = std::move(piece(i, std::move(piece_hash), piece_length));
 		offset += SHA_DIGEST_LENGTH;
 	}
+
+	// build work queue
+	for(auto& piece : m_pieces){
+		m_work_queue.push(&piece);
+	}
+
 };
 
 piece_manager& piece_manager::operator=(piece_manager && other){
 	m_pieces = std::move(other.m_pieces);
+	m_work_queue = std::move(other.m_work_queue);
 	return *this;
 };
 
