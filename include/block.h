da9ba56c16502c68d882df6f722840574e58852c
dimy20 <enzostrindade16@outlook.com>
2022-08-24 18:36:59 -0300

    keep it simple

--- include/block.h
+++ include/block.h
@@ -2,24 +2,14 @@
 #include "buffer.h"
 #define BLOCK_SIZE 1024 * 16
 
-class block{
-	friend class piece;
-	public:
-		block();
-		block(block && other);
-		block(const block& other);
-		block& operator=(block && other);
-		block& operator=(const block& other);
+typedef struct block_s{
+	// block data
+	char * m_data = nullptr;
+	// piece index
+	int m_index;
+	// byte offset within the piece
+	int m_offset;
+	// block size
+	int m_size;
+}block_t;
 
-		block(char * buffer, int size, int index, int offset);
-		int index(){ return m_index; };
-		int offset(){ return m_piece_offset; };
-
-	private:
-		// block of data, subset of the piece specified by index
-		buffer m_buffer;
-		// piece index
-		int m_index;
-		// byte offset within the piece
-		int m_piece_offset;
-};
